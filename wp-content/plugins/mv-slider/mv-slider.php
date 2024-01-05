72e2e63182d7c0989bf88e1f2b354a0a7023f777
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-05 17:21:07 -0300

    add_menus_admin2

--- wp-content/plugins/mv-slider/mv-slider.php
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -70,6 +70,26 @@ if( ! class_exists( 'MV_Slider' ) ){
                 array( $this, 'mv_slider_settings_page' ),
                 'dashicons-images-alt2',
             );
+
+            add_submenu_page(
+                'mv_slider_admin',
+                'Manage Slides',
+                'Manage Slides',
+                'manage_options',
+                'edit.php?post_type=mv-slider',
+                null,
+                null
+            );
+
+            add_submenu_page(
+                'mv_slider_admin',
+                'Add New Slides',
+                'Add New Slides',
+                'manage_options',
+                'post-new.php?post_type=mv-slider',
+                null,
+                null
+            );
         }
 
         public function mv_slider_settings_page() {
